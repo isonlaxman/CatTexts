2e1674788a126f34dd596f95056b6c8e4745e6ad
isonlaxman <32971829+isonlaxman@users.noreply.github.com>
2018-11-17 13:50:34 -0500

    Google API init

--- search.py
+++ search.py
@@ -1,6 +1,22 @@
 from google import google, images
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+driver = webdriver.Firefox(firefox_binary=binary)
 
 
 def setup_options():
-    options = images.ImageOptions();
-    #edi
+    options = images.ImageOptions()
+    options.size_category = "large"
+
+    return options
+
+
+def search(query="", options={}):
+    results = google.search_images(query, options)
+    print(results[1])
+
+
+options = setup_options()
+search(query="cats", options=options)
